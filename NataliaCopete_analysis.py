64d07f7d27181826a684bd7f753d6808a11ec940
omar <alexandroszapata.oazp@gmail.com>
2018-03-21 15:27:20 -0500

    hola2

--- NataliaCopete_analysis.py
+++ NataliaCopete_analysis.py
@@ -3,10 +3,10 @@ import matplolib.pyplot as plt
 
 #retorna distribucion normal
 def normal_dist(x,prom,sigma):
-  return np.random.normal(prom,sigma)
+  return np.sqrt(1/(2*np.sigma**2))*np.exp((-(x-prom)**2)/(sigma**2))
 
 def get_fit(filename):
   datos=np.loadtxt(filename)
   plt.hist(datos)
-  desv_valores=std(valores_posibles)
-  print normal_dist(promedi_valores,desv_valores)
+  
+  print normal_dist()
